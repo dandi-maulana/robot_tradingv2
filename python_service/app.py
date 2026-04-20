a95eb08ba851d1b3840eadd900dd30c5a6af3b25
root <root@localhost.localdomain>
2026-04-20 12:23:39 +0700

    JANGAN LUPA GANTI NAMA DATABASE DI APP.PY

--- python_service/app.py
+++ python_service/app.py
@@ -40,18 +40,18 @@ def _get_start_all_job_id():
 
 
 # --- KONFIGURASI MYSQL ---
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "robot_trading5",
-}
-# DB_CONFIG = {
-#     'host': 'localhost',
-#     'user': 'rodis_admin',
-#     'password': '@Nightmare02',
-#     'database': 'robot_trading'
-# }
+#DB_CONFIG = {
+#    "host": "localhost",
+#    "user": "root",
+#    "password": "",
+#    "database": "robot_trading5",
+#}
+ DB_CONFIG = {
+     'host': 'localhost',
+     'user': 'rodis_admin',
+     'password': '@Nightmare02',
+     'database': 'robot_trading'
+ }
 
 
 def get_db_connection():
